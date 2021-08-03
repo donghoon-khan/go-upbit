9df0526c1266799ecae530e4c4b5753816307b74
donghoon-khan <mustwin@kakao.com>
2021-08-03 21:45:53 +0900

    feature: webSocket IF 변경

--- client.go
+++ client.go
@@ -1,24 +1,68 @@
-package goupbit
+package main
 
 import (
-	"net/http"
+	"log"
+	"os"
+	"os/signal"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
 
-const (
-	UPBIT_BASE_URL = "https://api.upbit.com/v1"
-)
+var done chan interface{}
+var interrupt chan os.Signal
 
-type client struct {
-	baseURL    string
-	httpClient *http.Client
+func receiveHandler(connection *websocket.Conn) {
+	defer close(done)
+	for {
+		_, msg, err := connection.ReadMessage()
+		if err != nil {
+			log.Println("Error in receive:", err)
+			return
+		}
+		log.Printf("Received: %s\n", msg)
+	}
 }
 
-func newClient() *client {
-	return &client{
-		baseURL: UPBIT_BASE_URL,
-		httpClient: &http.Client{
-			Timeout: time.Second,
-		},
+func main() {
+	done = make(chan interface{})
+	interrupt = make(chan os.Signal)
+
+	signal.Notify(interrupt, os.Interrupt)
+
+	socketUrl := "wss://api.upbit.com/websocket/v1"
+	conn, _, err := websocket.DefaultDialer.Dial(socketUrl, nil)
+	if err != nil {
+		log.Fatal("Error connecting to Websocket Server:", err)
+	}
+	defer conn.Close()
+	go receiveHandler(conn)
+
+	for {
+		select {
+		case <-time.After(time.Duration(1) * time.Millisecond * 1000):
+			err := conn.WriteMessage(websocket.TextMessage, []byte(`[{"ticket":"test"},{"type":"ticker","codes":["KRW-BTC"]}]`))
+			if err != nil {
+				log.Println("Error during writing to websocket:", err)
+				return
+			}
+
+		case <-interrupt:
+			log.Println("Received SIGINT interrupt signal. Closing all pending connections")
+
+			err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("Error during closing websocket:", err)
+				return
+			}
+
+			select {
+			case <-done:
+				log.Println("Receiver Channel Closed! Exiting....")
+			case <-time.After(time.Duration(1) * time.Second):
+				log.Println("Timeout in closing receiving channel. Exiting....")
+			}
+			return
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/donghoon-khan/go-upbit
 
 go 1.16
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/jarcoal/httpmock v1.0.8 // indirect
+)
