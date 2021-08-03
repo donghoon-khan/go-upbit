179fc5228fea4f2584200597388ae449a4ab71f8
donghoon-khan <mustwin@kakao.com>
2021-08-03 23:29:47 +0900

    feature: gui menu 추가

---
+++ cmd/go-upbit/menu/welcome.go
@@ -0,0 +1,42 @@
+package menu
+
+import (
+	"net/url"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/cmd/fyne_demo/data"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func parseURL(urlStr string) *url.URL {
+	link, err := url.Parse(urlStr)
+	if err != nil {
+		fyne.LogError("Could not parse URL", err)
+	}
+
+	return link
+}
+
+func welcomeScreen(_ fyne.Window) fyne.CanvasObject {
+	logo := canvas.NewImageFromResource(data.FyneScene)
+	logo.FillMode = canvas.ImageFillContain
+	if fyne.CurrentDevice().IsMobile() {
+		logo.SetMinSize(fyne.NewSize(171, 125))
+	} else {
+		logo.SetMinSize(fyne.NewSize(228, 167))
+	}
+
+	return container.NewCenter(container.NewVBox(
+		widget.NewLabelWithStyle("Upbit 자동매매 프로그램 Demo", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+		logo,
+		container.NewHBox(
+		//widget.NewHyperlink("fyne.io", parseURL("https://fyne.io/")),
+		//widget.NewLabel("-"),
+		//widget.NewHyperlink("documentation", parseURL("https://fyne.io/develop/")),
+		//widget.NewLabel("-"),
+		//widget.NewHyperlink("sponsor", parseURL("https://github.com/sponsors/fyne-io")),
+		),
+	))
+}
